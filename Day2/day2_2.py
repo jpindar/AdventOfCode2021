1a3ad02af5a2b65190dbc805fe8db91f3ef2a0a1
Jeanne Pindar <jpindar@jpindar.com>
2021-12-10 13:05:26 -0500

    day 2

---
+++ Day2/day2_2.py
@@ -0,0 +1,36 @@
+"""
+AdventOfCode 2021 Day 2
+"""
+
+filename = "AdventOfCode/Day2/input.txt"
+# filename = "AdventOfCode/Day2/test_data.txt"
+data = []
+
+def read_file():
+    inputFile = open(filename, 'r')
+    with inputFile as f:
+        for s in f:
+            s = s.strip('\n\r')
+            data.append(s)
+
+
+def main():
+    x = 0
+    y = 0
+    aim = 0
+    read_file()
+    for i in range(len(data)):
+       d = int(data[i].split(" ")[1])
+       if "up" in data[i]:
+           aim -= d
+       elif "down" in data[i]:
+           aim += d
+       elif "forward" in data[i]:
+            x += d
+            y += (aim * d)
+    print(f"horizontal position {x}, depth {y}, result {x*y}")
+
+
+main()
+
+
